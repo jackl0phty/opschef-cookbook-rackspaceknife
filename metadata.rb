b890539aad5e00c872c95659accf76e2d4450b29
ghevener@customink.com <ghevener@customink.com>
2013-07-20 18:35:34 -0400

    Bump so we can upload to community.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "jackl0phty@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures rackspaceknife"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.8"
+version          "0.0.10"
 
 %w{ perl minitest-handler }.each do |ckbk|
   depends ckbk
